41c3bcba1b9796a431a7e79c3554cb5d32155fcd
keerthanah21 <120969927+keerthanah21@users.noreply.github.com>
2022-12-18 23:24:44 +0530

    Add files via upload

---
+++ naturalno.java
@@ -0,0 +1,11 @@
+package assignment;
+public class naturalno {
+	public static void main(String args[]) {
+	System.out.println("first 10 naturals no are :");
+	int i;
+	for(i=1;i<=10;i++)
+	{
+	System.out.println(i);
+	}
+	}
+}
