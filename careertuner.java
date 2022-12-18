64e3d4113a5faf9fe5c2c5d3987f2539044f10ee
keerthanah21 <120969927+keerthanah21@users.noreply.github.com>
2022-12-18 23:20:02 +0530

    Add files via upload

---
+++ careertuner.java
@@ -0,0 +1,42 @@
+package assignment5;
+import java.util.Scanner;
+public class careertuner
+{
+	//take three numbers from the user and print the greatest number
+	public void greatestnum()
+	{
+		int b,c;
+		Float a;
+		Scanner h=new Scanner(System.in);
+		System.out.println("the greatest number of 3");
+		System.out.println("enter the 1st number");
+		a=h.nextFloat();
+		System.out.println("enter the 2nd number");
+		b=h.nextInt();
+		System.out.println("enter the 3 rd number");
+		c=h.nextInt();
+		if(a>b&&a>c)
+		{
+			System.out.println("the greatest number"+a);
+		}
+		else if(b>c)
+		{
+			System.out.println("the greatest number"+b);
+		
+		}
+		else
+		{
+			System.out.println("the greatest number"+c);
+		}
+		}
+	
+	public static void main(String[] args) 
+	{
+	careertuner obj=new careertuner();
+	obj.greatestnum();
+	
+		
+
+	}
+
+}
